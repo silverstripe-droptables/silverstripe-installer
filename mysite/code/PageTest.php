88a3cfa8ba912a38849fb2c7741ca041879bb35c
Paul Clarke <paul@silverstripe.com>
2012-06-01 10:51:55 +1200

    FEATURE: test page type for template development

---
+++ mysite/code/PageTest.php
@@ -0,0 +1,9 @@
+<?php
+/* This page is purely for testing generic page styles and can be removed once in production */
+class PageTest extends Page {
+
+}
+class PageTest_Controller extends Page_Controller {
+
+}
+
