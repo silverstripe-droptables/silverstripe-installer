4eaff3aa380ee3bfb91a8e6db0084caa47b8453e
Mateusz Uzdowski <mateusz@silverstripe.com>
2012-06-01 15:19:48 +1200

    BUGFIX: fix the macron path so the TinyMCE works

--- mysite/_config.php
+++ mysite/_config.php
@@ -22,7 +22,7 @@ HtmlEditorConfig::get('cms')->setOption('theme_advanced_blockformats', 'p,pre,ad
 // Add in start and type attributes for ol
 HtmlEditorConfig::get('cms')->setOption('extended_valid_elements', 'img[class|src|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|usemap],iframe[src|name|width|height|title|align|allowfullscreen|frameborder|marginwidth|marginheight|scrolling],object[width|height|data|type],param[name|value],map[class|name|id],area[shape|coords|href|target|alt],ol[start|type]');
 // Macrons
-HtmlEditorConfig::get('cms')->enablePlugins(array('ssmacron' => '../../../cms/javascript/tinymce_ssmacron/editor_plugin_src.js'));
+HtmlEditorConfig::get('cms')->enablePlugins(array('ssmacron' => '../../../framework/thirdparty/tinymce_ssmacron/editor_plugin_src.js'));
 HtmlEditorConfig::get('cms')->insertButtonsAfter('charmap', 'ssmacron');
 
 Email::setAdminEmail('info@localhost');
@@ -33,3 +33,4 @@ GD::set_default_quality(90);
 
 FulltextSearchable::enable();
 
+Object::add_extension('SiteConfig', 'CustomSiteConfig');
