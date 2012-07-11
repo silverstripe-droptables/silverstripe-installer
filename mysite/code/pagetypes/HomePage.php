d96513837efa4533e42ff6eead0fe34cc627f70c
Robert Curry <robert@silverstripe.com>
2012-07-11 14:49:45 +1200

    BUG: Fix NewsItems() function name on HomePage_Controller (METUS-15)

--- mysite/code/pagetypes/HomePage.php
+++ mysite/code/pagetypes/HomePage.php
@@ -4,11 +4,11 @@ class HomePage extends Page {
 }
 class HomePage_Controller extends Page_Controller {
 
-	public function NewsItems() {
+	public function getNewsItems() {
 		$newsHolder = NewsHolder::get_one('NewsHolder');
 		if ($newsHolder) {
 			$controller = new NewsHolder_Controller($newsHolder);
-			return $controller->NewsItems(5);
+			return $controller->getNewsItems(5);
 		}
 	}
 
