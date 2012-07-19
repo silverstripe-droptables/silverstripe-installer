4f2fad1721c469632d1dba5fad9357dd3a8de543
Robert Curry <robert@silverstripe.com>
2012-07-19 13:10:50 +1200

    Trim down JS in Page

--- mysite/code/pagetypes/Page.php
+++ mysite/code/pagetypes/Page.php
@@ -23,11 +23,35 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 
+		$themeDir = SSViewer::get_theme_folder();
+		Requirements::clear();
 		Requirements::combine_files(
 			'ssexpress.js',
 			array(
-				'themes/ssexpress/js/general.js',
-				'themes/ssexpress/js/express.js'
+				"$themeDir/js/lib/modernizr.js",
+				"$themeDir/js/lib/jquery.js",
+				'themes/module_bootstrap/js/bootstrap-transition.js',
+//				'themes/module_bootstrap/js/bootstrap-alert.js',
+//				'themes/module_bootstrap/js/bootstrap-modal.js',
+//				'themes/module_bootstrap/js/bootstrap-scrollspy.js',
+//				'themes/module_bootstrap/js/bootstrap-tab.js',
+//				'themes/module_bootstrap/js/bootstrap-tooltip.js',
+//				'themes/module_bootstrap/js/bootstrap-popover.js',
+//				'themes/module_bootstrap/js/bootstrap-button.js',
+				'themes/module_bootstrap/js/bootstrap-collapse.js',
+				'themes/module_bootstrap/js/bootstrap-carousel.js',
+//				'themes/module_bootstrap/js/bootstrap-typeahead.js',
+				"$themeDir/js/general.js",
+				"$themeDir/js/express.js",
+				"$themeDir/js/forms.js"
+			)
+		);
+		Requirements::combine_files(
+			'ssexpress.css',
+			array(
+				"$themeDir/css/layout.css",
+				"$themeDir/css/typography.css",
+				"$themeDir/css/forms.css"
 			)
 		);
 	}
