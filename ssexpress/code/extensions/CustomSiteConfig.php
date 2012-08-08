a432a5208543d307e5cb1c15d95983dc66ddbcb0
Robert Curry <robert@silverstripe.com>
2012-08-08 16:48:08 +1200

    Add AddThis option to CustomSiteConfig

--- ssexpress/code/extensions/CustomSiteConfig.php
+++ ssexpress/code/extensions/CustomSiteConfig.php
@@ -7,7 +7,8 @@ class CustomSiteConfig extends DataExtension {
 	function extraStatics($class = null, $extension = null) {
 		return array(
 			'db' => array(
-				'GACode' => 'Varchar(16)'
+				'GACode' => 'Varchar(16)',
+				'AddThisProfileID' => 'Varchar(32)'
 			)
 		);
 	}
@@ -15,5 +16,8 @@ class CustomSiteConfig extends DataExtension {
 	function updateCMSFields(FieldList $fields) {
 		$fields->addFieldToTab('Root.Main', $gaCode = new TextField('GACode', 'Google Analytics account'));
 		$gaCode->setRightTitle('Account number to be used all across the site (UA-XXXXX-X)');
+
+		$fields->addFieldToTab('Root.Main', $addThisID = new TextField('AddThisProfileID', 'AddThis Profile ID'));
+		$addThisID->setRightTitle('Profile ID to be used all across the site (ra-XXXXXXXXXXXXXXXX)');
 	}
 }
