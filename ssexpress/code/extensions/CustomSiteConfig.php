ae58146fca7edacd70f05e4dbda0ea677fcd17bf
Robert Curry <robert@silverstripe.com>
2012-08-17 16:28:01 +1200

    Admins can edit Facebook and Twitter links.

--- ssexpress/code/extensions/CustomSiteConfig.php
+++ ssexpress/code/extensions/CustomSiteConfig.php
@@ -8,6 +8,8 @@ class CustomSiteConfig extends DataExtension {
 		return array(
 			'db' => array(
 				'GACode' => 'Varchar(16)',
+				'FacebookURL' => 'Varchar(256)', // multitude of ways to link to Facebook accounts, best to leave it open.
+				'TwitterUsername' => 'Varchar(16)', // max length of Twitter username 15
 				'AddThisProfileID' => 'Varchar(32)'
 			)
 		);
@@ -15,9 +17,15 @@ class CustomSiteConfig extends DataExtension {
 
 	function updateCMSFields(FieldList $fields) {
 		$fields->addFieldToTab('Root.Main', $gaCode = new TextField('GACode', 'Google Analytics account'));
-		$gaCode->setRightTitle('Account number to be used all across the site (UA-XXXXX-X)');
+		$gaCode->setRightTitle('Account number to be used all across the site (in the format <strong>UA-XXXXX-X</strong>)');
+
+		$fields->addFieldToTab('Root.Main', $gaCode = new TextField('FacebookURL', 'Facebook UID or username'));
+		$gaCode->setRightTitle('Facebook link (everything after the "http://facebook.com/", eg http://facebook.com/<strong>username</strong> or http://facebook.com/<strong>pages/108510539573</strong>)');
+
+		$fields->addFieldToTab('Root.Main', $gaCode = new TextField('TwitterUsername', 'Twitter username'));
+		$gaCode->setRightTitle('Twitter username (eg, http://twitter.com/<strong>username</strong>)');
 
 		$fields->addFieldToTab('Root.Main', $addThisID = new TextField('AddThisProfileID', 'AddThis Profile ID'));
-		$addThisID->setRightTitle('Profile ID to be used all across the site (ra-XXXXXXXXXXXXXXXX)');
+		$addThisID->setRightTitle('Profile ID to be used all across the site (in the format <strong>ra-XXXXXXXXXXXXXXXX</strong>)');
 	}
 }
